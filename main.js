b148a9d35c4748c7ce0dfdc6736742aa12095fde
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2021-08-02 11:33:17 +0530

    style: basic-styles

---
+++ main.js
@@ -0,0 +1,5 @@
+const billAmount = document.querySelector("#bill-amount");
+const amountCheckBtn = document.querySelector(".check-amount-btn");
+const cashGiven = document.querySelector("#cash-given");
+const checkChangeBtn = document.querySelector("return-change-btn");
+const messageArea = document.querySelector(".message-area");
